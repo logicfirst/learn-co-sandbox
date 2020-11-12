a51b7f3c41e3daf28c40ce7af6704cd5755fd0dc
Scott H Lee <slee@logicfirstllc.com>
2020-11-12 17:25:42 +0000

    Automatically backed up by Learn

---
+++ hashes.rb
@@ -0,0 +1,14 @@
+english_bands_by_city = {
+  :manchester => [
+    {
+      :band_name => "The Smiths",
+      :member_names => 
+      ["Morrissey", "Johnny", "Andy", "Mike"]
+    },
+    {
+      :band_name => "Joy Division",
+      :member_names => 
+      ["Peter", "Steven", "Bernard", "Ian"]
+    }
+    ]
+}
---
+++ learning-comments.rb
@@ -0,0 +1,4 @@
+name = "Byron"
+#name = "Luca"
+
+puts "We're sorry, but per the health inspector's rules, #{name} is not allowed in the store"
